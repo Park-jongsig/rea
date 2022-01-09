850ceed27b1102f71da888c4452c2babeb51ae1f
jong <zasmedia@naver.com>
2022-01-09 21:07:18 +0900

    first commit

---
+++ src/component/Hello.js
@@ -0,0 +1,16 @@
+// const Hello = () => {
+//     <p>Hello</p>;
+// };
+
+// export default Hello;
+import World from "./World"
+export default function Hello(){
+    return (
+        <div>
+            <h1>Hello</h1>
+            <World/>
+        </div>
+    )
+        
+    
+}
